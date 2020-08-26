8401c725267252eba8789cd927730ff4db73466d
Jeffsitorus <jefrisitorus22@gmail.com>
2020-08-27 00:03:20 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('login', 'Auth\LoginController@showFormLogin')->name('login');
+Route::get('register', 'Auth\RegisterController@showFormRegister')->name('register');
+Route::get('/dashboard', 'DashboardController@index');
