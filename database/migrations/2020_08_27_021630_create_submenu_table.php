4e2f007ca739e42a6dacd9d5766423e24504033a
Jeffsitorus <jefrisitorus22@gmail.com>
2020-08-27 09:23:34 +0700

    migration table submenu serta model dan controller

---
+++ database/migrations/2020_08_27_021630_create_submenu_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubmenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('submenu', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('menu_id')->constrained('menu');
+            $table->string('judul', 191);
+            $table->string('slug', 191);
+            $table->string('url');
+            $table->string('icon');
+            $table->tinyInteger('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('submenus');
+    }
+}
